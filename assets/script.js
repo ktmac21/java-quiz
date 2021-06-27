572854026291adb479ce951e54e0ca1db4993de0
Katie Eagles <kteagles21@gmail.com>
2021-06-27 00:21:19 -0400

    Improved CSS and added quiz javaacode.

--- assets/script.js
+++ assets/script.js
@@ -1,170 +1,188 @@
-// var quizQuestions = [
+const quizQuestions = [
 
-//     {
-        
-//     question: "Values made up of text & can contain letters, numbers, symbols, punctuation, & even emojis are called",
-//     answers: {
+  {
 
-//     a: "strings",
-// 	b: "methods",
-// 	c: "functions",
-// 	d: "text messages",
-// },
+    question: "Values made up of text & can contain letters, numbers, symbols, punctuation, & even emojis are called:",
+    answers: {
+      a: "strings",
+      b: "methods",
+      c: "functions",
+      d: "text messages",
+    },
+    correctAnswer: "a"
+  },
 
-// correctAnswer: "a"
-// },
+  {
 
-// {
-        
-//     question: "This property keeps track of how many characters it has:",
-//     answers: {
+    question: "This property keeps track of how many characters it has:",
+    answers: {
+      a: "string",
+      b: "length",
+      c: "slice",
+      d: "semi-colon",
+    },
 
-//     a: "string",
-// 	b: "length",
-// 	c: "slice",
-// 	d: "semi-colon",
-// },
-
-// correctAnswer: "b"
-// },
+    correctAnswer: "b",
+  },
 
 
-// {
-        
-//     question: "In Javascript, these are variables that contain multiple data values and use keys to name values:",
-//     answers: {
+  {
+    question: "In Javascript, these are variables that contain multiple data values and use keys to name values:",
+    answers: {
+      a: "functions",
+      b: "methods",
+      c: "data",
+      d: "objects",
+    },
+    correctAnswer: "d",
+  },
 
-//     a: "functions",
-// 	b: "methods",
-// 	c: "data",
-// 	d: "objects",
-// },
+  {
 
-// correctAnswer: "d"
-// },
+    question: "This is the word for functions when they are stored as a property within a JavaScript object:",
+    answers: {
+      a: "mediaQuery",
+      b: "methods",
+      c: "functions",
+      d: "event listeners",
+    },
+    correctAnswer: "b",
+  },
 
-// {
-        
-//     question: "This is the word for functions when they are stored as a property within a JavaScript object:",
-//     answers: {
+  {
 
-//     a: "mediaQuery",
-// 	b: "methods",
-// 	c: "functions",
-// 	d: "event listeners",
-// },
+    question: "If a condition is true, this conditional statement is used to specify execution for a block of code:",
+    answers: {
+      a: "if",
+      b: "while",
+      c: "if else",
+      d: "none of the above",
+    },
+    correctAnswer: "a",
+  },
 
-// correctAnswer: "b"
-// },
+  {
 
-// {
-        
-//     question: "If a condition is true, this conditional statement is used to specify execution for a block of code:",
-//     answers: {
+    question: "This conditional statement specifies a new test if the first condition is false:",
+    answers: {
+      a: "if else",
+      b: "while",
+      c: "for loop",
+      d: "when",
+    },
+    correctAnswer: "a",
+  },
 
-//     a: "if",
-// 	b: "while",
-// 	c: "if else",
-// 	d: "none of the above",
-// },
+  {
 
-// correctAnswer: "a"
-// },
+    question: "A function must be what in order to call it:",
+    answers: {
+      a: "declared",
+      b: "imagined",
+      c: "enabled",
+      d: "run",
+    },
+    correctAnswer: "a",
 
-// {
-        
-//     question: "This conditional statement specifies a new test if the first condition is false:",
-//     answers: {
+  },
 
-//     a: "if else",
-// 	b: "while",
-// 	c: "for loop",
-// 	d: "when",
-// },
+  {
+    question: "A boolean value is a value that can be either:",
+    answers: {
+      a: "number or letter",
+      b: "true or false",
+      c: "right or wrong",
+      d: "All of the above",
+    },
+    correctAnswer: "b",
 
-// correctAnswer: "a"
-// },
+  },
 
-// {
-        
-//     question: "A function must be what in order to call it?:",
-//     answers: {
+  {
 
-//     a: "declared",
-// 	b: "imagined",
-// 	c: "enabled",
-// 	d: "run",
-// },
+    question: "This Javascript function is used to detect certain events, such as a user's mouse click or a key press, and then it responds accordingly:",
+    answers: {
+      a: "event detector",
+      b: "movement listener",
+      c: "event listener",
+      d: "None of the above",
+    },
+    correctAnswer: "b",
 
-// correctAnswer: "a"  
+  },
 
-// },
+  {
 
-// {
-        
-//     question: "A boolean value is a value that can be either:",
-//     answers: {
+    question: "Javascript’s querySelector() method does which of the following:",
+    answers: {
+      a: "returns the first element that matches a specified CSS selector(s) in the document",
+      b: "runs a query loop after it’s selected",
+      c: "starts the program when an HTML element is clicked",
+      d: "runs a while loop",
+    },
+    correctAnswer: "a",
 
-//     a: "number or letter",
-// 	b: "true or false",
-// 	c: "right or wrong",
-// 	d: "All of the above",
-// },
+  },
 
-// correctAnswer: "b"  
+]
 
-// },
-
-// {
-        
-//     question: "This Javascript function is used to detect certain events, such as a user's mouse click or a key press, and then it responds accordingly:",
-//     answers: {
-
-//     a: "event detector",
-// 	b: "movement listener",
-// 	c: "event listener",
-// 	d: "None of the above",
-// },
-
-// correctAnswer: "b"  
-
-// },
-
-// {
-        
-//     question: "Javascript’s querySelector() method does which of the following:",
-
-//     answers: {
-
-//     a: "returns the first element that matches a specified CSS selector(s) in the document",
-// 	b: "runs a query loop after it’s selected",
-// 	c: "starts the program when an HTML element is clicked",
-// 	d: "runs a while loop",
-// },
-
-// correctAnswer: "a"  
-
-// },
-
-// ]
-
+var startButton = document.querySelector("#begin-quiz");
+var timerText = document.querySelector("#timer");
+var quizContainer = document.querySelector("#quiz");
+var scoreText = document.querySelector("#player-score");
+var highScoreText = document.querySelector("#high-score");
 var secondsLeft = 30;
-var startButton = document.querySelector(".button");
-var timerText = document.querySelector(".timer");
-startButton.addEventListener("click",function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timerText.textContent = secondsLeft;
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
+
+
+
+
+function startQuiz() {
+
+  const output = [];
+
+  quizQuestions.forEach(
+    (currentQuestion, questionNumber) => {
+      
+      const answers = [];
+
+      for (letter in currentQuestion.answers) {
+
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
       }
-    }, 1000);
-  }
-  )
+
+      output.push(
+      `<div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join('')} </div>`
+      );
+    }
+  );
+
+  quizContainer.innerHTML = output.join('');
+}
+
+startButton.addEventListener("click", function setTime() {
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    timerText.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+  }, 1000);
+}
+)
+
 function sendMessage() {
-    timerText.textContent = "Your time is up!";
-  }
+  timerText.textContent = "Your time is up!";
+
+};
+
+startButton.addEventListener("click", startQuiz);
+
+
